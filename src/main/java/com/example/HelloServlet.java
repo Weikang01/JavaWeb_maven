519bad8a811480dc81d71a0be25c3ce375ed70a2
Weikang01 <lwkdota@qq.com>
2021-07-17 23:37:18 -0400

    After Servlet Loading

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet</h1>");
+        out.println("<h1>你好 Servlet</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
