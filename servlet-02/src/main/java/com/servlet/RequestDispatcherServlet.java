9ac7552a37d9be7415bc504b8f5b1750f72ec17f
Weikang01 <lwkdota@qq.com>
2021-07-18 09:04:18 -0400

    After Read Data from Properties

---
+++ servlet-02/src/main/java/com/servlet/RequestDispatcherServlet.java
@@ -0,0 +1,22 @@
+package com.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RequestDispatcherServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext context = this.getServletContext();
+        context.getRequestDispatcher("/gp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
